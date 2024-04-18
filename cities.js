d9ae0ee46660b63d59abf689898da796084b3854
Juan Perez <juan.eperez88@gmail.com>
2024-04-18 09:54:02 -0400

    Se actualiza documento cities con los datos requeridos

---
+++ cities.js
@@ -0,0 +1,30 @@
+const barcelona = {
+    titulo: 'Descubre Barcelona: La joya de Cataluña',
+    subtitulo: 'Sumérgete en la historia, cultura y belleza de esta vibrante ciudad.',
+    parrafo: "Bienvenidos a Barcelona, una ciudad cautivadora que combina a la perfección la riqueza."
+}
+
+const roma = {
+    titulo: 'Roma: Tesoros eternos enla Ciudad Eterna',
+    subtitulo: '',
+    parrafo: ""
+}
+
+const paris = {
+    titulo: 'París: La Ciudad de la luz y el romance.',
+    subtitulo: '',
+    parrafo: ""
+}
+
+const londres = {
+    titulo: 'Londres: Descubre la magia de la ciudad del Támesis.',
+    subtitulo: '',
+    parrafo: ""
+}
+
+export {
+    barcelona,
+    roma,
+    paris,
+    londres
+}
